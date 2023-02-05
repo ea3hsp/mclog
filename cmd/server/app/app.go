3e139106d40608547ffe096a6bbe8ca42e1ea27e
aespinro <aespin@gcelsa.com>
2023-02-05 18:44:01 +0100

    first commit

---
+++ cmd/server/app/app.go
@@ -0,0 +1,59 @@
+package app
+
+import (
+	vercmd "github.com/ea3hsp/mclog/cmd/server/commands/version"
+	"github.com/ea3hsp/mclog/version"
+	"github.com/urfave/cli/v2"
+)
+
+var extraCmds = []*cli.Command{}
+
+// New returns a *cli.App instance.
+func New() *cli.App {
+	app := cli.NewApp()
+	app.Name = "server"
+	app.Version = version.Version
+	app.Usage = "mclog server"
+	app.Description = "mclog server"
+
+	app.EnableBashCompletion = true
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:    "api-addr",
+			Usage:   "API IP bind address",
+			Value:   "0.0.0.0",
+			Aliases: []string{"aa"},
+			EnvVars: []string{"API_ADDR"},
+		},
+		&cli.IntFlag{
+			Name:    "api-port",
+			Usage:   "API TCP IP port",
+			Value:   3000,
+			Aliases: []string{"ap"},
+			EnvVars: []string{"API_PORT"},
+		},
+		&cli.StringFlag{
+			Name:    "log-level",
+			Usage:   "log level",
+			Value:   "debug",
+			Aliases: []string{"ll"},
+			EnvVars: []string{"LOG_LEVEL"},
+		},
+	}
+	app.Commands = append([]*cli.Command{
+		// version command
+		vercmd.Command,
+		// start command
+		startcmd.Command,
+	}, extraCmds...)
+	app.Before = func(ctx *cli.Context) error {
+		return nil
+	}
+	app.Authors = []*cli.Author{
+		{
+			Name:  "Albert Espín",
+			Email: "aespin@gcelsa.com",
+		},
+	}
+	return app
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,23 @@
 module github.com/ea3hsp/mclog
 
 go 1.19
+
+require (
+	google.golang.org/grpc v1.52.3
+	google.golang.org/protobuf v1.28.1
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/urfave/cli/v2 v2.24.3
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	google.golang.org/genproto v0.0.0-20221118155620-16455021b5e6 // indirect
+)
